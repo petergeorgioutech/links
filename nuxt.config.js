8b5783240ef0d51ff83adab3b1a63d57945d300a
Panayiotis Georgiou <panayiotisgeorgiou@Panayiotiss-MacBook-Pro.local>
2022-01-16 08:12:53 +0200

    added gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,16 @@ export default {
         src: "https://code.jquery.com/jquery-3.6.0.min.js",
       },
       {
+        hid: 'tagmanager-script',
+        innerHTML: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PNK4H79');
+        `
+      },
+      {
         hid: 'gtm-script1',
         src: 'https://www.googletagmanager.com/gtag/js?id=G-M9YSNPHB2S',
         defer: true,
