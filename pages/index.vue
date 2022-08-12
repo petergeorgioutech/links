72e6f4cdfa7f43d51c51cbf77ed0e65dd32efa55
Peter Georgiou <peter@petergeorgiou.tech>
2022-08-12 13:03:17 +0300

    add links better

--- pages/index.vue
+++ pages/index.vue
@@ -14,8 +14,8 @@
             <h1 class="username">Peter Georgiou</h1>
             <p class="description">Frontend Web Developer <span class="small-type">@peterpandev</span></p>
             <ul class="socials-wrapper">
-              <li><a href="//instagram.com/peterpandev" :style="instagramImage"><span class="icon-instagram"></span></a></li>
-              <li><nuxt-link to="/youtube" :style="youtubeImage"><span class="icon-youtube"></span></nuxt-link></li>
+              <li><a href="//instagram.com/peterpandev" target="blank" :style="instagramImage"><span class="icon-instagram"></span></a></li>
+              <li><a href="//petergeorgiou.tech/youtube" target="blank" :style="youtubeImage"><span class="icon-youtube"></span></a></li>
             </ul>
             <a href="mailto:hello@petergeorgiou.tech" class="status accepting">
               Accepting <span class="project_type">Freelance Projects</span>
