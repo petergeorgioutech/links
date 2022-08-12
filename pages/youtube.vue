fc9b4290190df3eb4c3066189579069c12ded953
Peter Georgiou <peter@petergeorgiou.tech>
2022-08-12 12:42:52 +0300

    added youtube deep link

---
+++ pages/youtube.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  head: {
+    script: [
+      {
+        hid: 'onload',
+        innerHTML: `
+          window.onload = function() {
+        var fallback = "https://youtube.com/channel/UCh_3XIVQLMvM50Z0xQltlwQ",
+        app = "vnd.youtube://channel/UCh_3XIVQLMvM50Z0xQltlwQ";
+
+        if( /Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+            window.location = app;
+            window.setTimeout(function() {
+                window.location = fallback;
+            }, 25);
+        } else {
+            window.location = fallback;
+        }
+
+        function killPopup() {
+            window.removeEventListener('pagehide', killPopup);
+        }
+
+        window.addEventListener('pagehide', killPopup);
+      };
+        `,
+        type: 'text/javascript',
+        charset: 'utf-8'
+      }
+    ]
+  },
+  layout: 'default',
+};
+</script>
