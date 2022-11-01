775d31bf58b32f2f2f755b289261a8f5fdd8c2d7
Peter Georgiou <hello@petergeorgiou.tech>
2022-11-01 11:14:57 +0200

    added newsletter sign up

--- pages/index.vue
+++ pages/index.vue
@@ -24,18 +24,12 @@
           </div>
         </div>
         <div class="row mb-3 featured-items">
+          <a v-b-modal.modal-1 class="featured-item" :style="ebookImage">
+            <span class="featured-title">E-Book Release ✨</span>
+          </a>
           <nuxt-link to="/setup" class="featured-item" :style="setupImage">
             <span class="featured-title">My Setup Gear</span>
           </nuxt-link>
-          <a href="https://bit.ly/3U95RU6" target="_blank" class="featured-item" :style="neonImage" style="background-position:center bottom;">
-            <span class="featured-title">Neon Beach</span>
-          </a>
-          <a href="http://www.quadlockcase.net/mag" target="_blank" class="featured-item" :style="quadlockImage">
-            <span class="featured-title">Quad Lock MAG&trade;</span>
-          </a>
-          <a href="https://academy.zerotomastery.io/p/complete-web-developer-zero-to-mastery?affcode=441520_wmgpaqt4" target="_blank" class="featured-item" :style="ztmImage">
-            <span class="featured-title">Zero To Mastery</span>
-          </a>
           <a href="https://vscodethemes.com/e/morgan-codes.morgan-codes-vscode-theme" target="_blank" class="featured-item" :style="vsCodeImage">
             <span class="featured-title">VS Code Theme</span>
           </a>
@@ -61,6 +55,53 @@
         </div>
       </div>
     </div>
+    <div>
+    <b-modal id="modal-1" title="Coming Soon ✨" hide-footer>
+      <div class="row">
+        <div class="col-12">
+          <img class="book-cover img-fluid" src="~/assets/img/ebooksnapshot.png" />
+          <h3 class="title text-center mb-3 h4 mt-3">Sign up below to be the first to know when the E-Book is released! </h3>
+        </div>
+        <div class="col-12">
+          <!-- Begin Mailchimp Signup Form -->
+          <link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css">
+          <div id="mc_embed_signup">
+              <form action="https://tech.us21.list-manage.com/subscribe/post?u=cef0d5f27e1a3959c4d32cea8&amp;id=caa488f38c&amp;f_id=0070c1e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll">
+          <div class="mc-field-group">
+            <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+          </label>
+            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
+            <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+          </div>
+          <div class="mc-field-group">
+            <label for="mce-FNAME">First Name </label>
+            <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+            <span id="mce-FNAME-HELPERTEXT" class="helper_text"></span>
+          </div>
+            <div id="mce-responses" class="clear foot">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cef0d5f27e1a3959c4d32cea8_caa488f38c" tabindex="-1" value=""></div>
+                  <div class="optionalParent">
+                      <div class="clear foot">
+                          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                          <p class="brandingLogo"><a href="http://eepurl.com/icGc9z" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+                      </div>
+                  </div>
+              </div>
+          </form>
+          </div>
+          <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+          <!--End mc_embed_signup-->
+        </div>
+      </div>
+
+    </b-modal>
+  
+</div>
+
   </div>
 </template>
 
@@ -78,6 +119,7 @@ import youtube from "assets/img/youtube.png";
 import tiktok from "assets/img/tiktok.png";
 import quadlock from "assets/img/brands/quadlock.jpg";
 import neon from "assets/img/brands/neonbeach.jpg";
+import ebook from "assets/img/ebooksnapshot.png";
 
 export default {
   layout: 'default',
@@ -97,6 +139,7 @@ export default {
       youtubeImage: { backgroundImage: `url(${youtube})` },
       quadlockImage: { backgroundImage: `url(${quadlock})` },
       neonImage: { backgroundImage: `url(${neon})` },
+      ebookImage: { backgroundImage: `url(${ebook})` },
     };
   }
 };
